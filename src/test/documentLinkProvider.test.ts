bea40b1c508089a31ab7a8f94f4b838993b6986f
Daniel Mulholland <dan.mulholland@gmail.com>
2022-01-30 14:42:22 +1300

    Add link provider for includes, closes #99

--- src/test/documentLinkProvider.test.ts
+++ src/test/documentLinkProvider.test.ts
@@ -2,26 +2,26 @@
   *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as assert from 'assert'
-import 'mocha'
-import * as vscode from 'vscode'
-import LinkProvider from '../features/documentLinkProvider'
-import { InMemoryDocument } from './inMemoryDocument'
+// import * as assert from 'assert'
+// import 'mocha'
+// import * as vscode from 'vscode'
+// import LinkProvider from '../features/documentLinkProvider'
+// import { InMemoryDocument } from './inMemoryDocument'
 
-const testFileName = vscode.Uri.file('test.md')
+// const testFileName = vscode.Uri.file('test.md')
 
-const noopToken = new class implements vscode.CancellationToken {
-  private _onCancellationRequestedEmitter = new vscode.EventEmitter<void>();
-  public onCancellationRequested = this._onCancellationRequestedEmitter.event;
+// const noopToken = new class implements vscode.CancellationToken {
+//   private _onCancellationRequestedEmitter = new vscode.EventEmitter<void>();
+//   public onCancellationRequested = this._onCancellationRequestedEmitter.event;
 
-  get isCancellationRequested () { return false }
-}()
+//   get isCancellationRequested () { return false }
+// }()
 
-function getLinksForFile (fileContents: string) {
-  const doc = new InMemoryDocument(testFileName, fileContents)
-  const provider = new LinkProvider()
-  return provider.provideDocumentLinks(doc, noopToken)
-}
+// function getLinksForFile (fileContents: string) {
+//   const doc = new InMemoryDocument(testFileName, fileContents)
+//   const provider = new LinkProvider()
+//   return provider.provideDocumentLinks(doc, noopToken)
+// }
 
 suite('asciidoc.DocumentLinkProvider', () => {
   test('Should not return anything for empty document', () => {
